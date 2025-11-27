fabd6cda82d81f48e6a3eac6e0e712dddc11acbd
JWK258 <rlawodnjs200@hs.ac.kr>
2025-11-28 00:18:06 +0900

    대시 관련 수정본

--- Assets/Scripts/SuperPlayerController64.cs
+++ Assets/Scripts/SuperPlayerController64.cs
@@ -12,6 +12,14 @@ public class SuperPlayerController64 : MonoBehaviour
     public float h;
     public float maxSpeed;
 
+    /* 추가된 변수들 */
+    public float dashPower = 15f;
+    public float dashTime = 0.2f;
+    private bool isDashing = false;
+    private int facingDir = 1;
+    private float defaultGravity;
+    /* 추가된 변수 */
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,21 +28,40 @@ public class SuperPlayerController64 : MonoBehaviour
         sprend = GetComponent<SpriteRenderer>(); // 뒤집기용
         h = 0f; // 이전 프레임 유도
 
+        defaultGravity = rigid.gravityScale;//시작할 때 설정된 중력값 저장
     }
 
     // Update is called once per frame
     void Update()
     {
+        /* 추가된 변수들 */
+        if (isDashing) return; //대쉬 중일때 조작 x
+        /* 추가된 변수들 */
+
         Vector2 vec;
-            float i = h; // 전 프레임 입력값 가져와서 회전 자연스럽게 하기
-            if (Input.GetButtonUp("Horizontal") || Input.GetButton("Horizontal")) // 스프라이트 뒤집기
-                sprend.flipX = i < 0;
-            h = Input.GetAxisRaw("Horizontal"); // 순서상 갱신 되기 전에 위의 i에서 값을 가져옴 -> 1프레임 짤짤이는 방향을 꼬아버릴 가능성. 다른 방식으로 플립하기 고려
-            vec = new Vector2(h*3, rigid.velocity.y);
+        float i = h; // 전 프레임 입력값 가져와서 회전 자연스럽게 하기
+        if (Input.GetButtonUp("Horizontal") || Input.GetButton("Horizontal")) // 스프라이트 뒤집기
+            sprend.flipX = i < 0;
+        h = Input.GetAxisRaw("Horizontal"); // 순서상 갱신 되기 전에 위의 i에서 값을 가져옴 -> 1프레임 짤짤이는 방향을 꼬아버릴 가능성. 다른 방식으로 플립하기 고려
+
+        /* 추가된 변수들 */
+        if (h > 0) facingDir = 1;//방향
+        else if (h < 0) facingDir = -1;
+        /* 추가된 변수들 */
+
+        /* 추가된 변수들 */
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            StartDash();
+            return;
+        }
+        /* 추가된 변수들 */
+
+        vec = new Vector2(h * 3, rigid.velocity.y);
         // rigid.AddForce(vec, ForceMode2D.Impulse); // 이동
         rigid.velocity = vec;
 
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             Vector2 vec2 = new Vector2(rigid.velocity.x, 10);
             rigid.velocity = vec2;
@@ -58,14 +85,38 @@ public class SuperPlayerController64 : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             anim.SetBool("Attack", true);
-        } else
+        }
+        else
             anim.SetBool("Attack", false);
 
     }
     void FixedUpdate()
     {
-        if (rigid.velocity.x > maxSpeed && anim.GetBool("Attack")==false) // 오른쪽이동
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y); 
+        /* 추가된 변수들 */
+        if (isDashing) return;//대쉬 중일 때 속도제한 x
+        /* 추가된 변수들 */
+
+        if (rigid.velocity.x > maxSpeed && anim.GetBool("Attack") == false) // 오른쪽이동
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x < -1 * maxSpeed && anim.GetBool("Attack") == false) //왼쪽이동
-            rigid.velocity = new Vector2(-1 * maxSpeed, rigid.velocity.y); }
+            rigid.velocity = new Vector2(-1 * maxSpeed, rigid.velocity.y);
+    }
+
+    /* 추가된 변수들 */
+    void StartDash()
+    {
+        isDashing = true;
+        defaultGravity = rigid.gravityScale;
+        rigid.velocity = new Vector2(facingDir * dashPower, 0);
+        rigid.gravityScale = 0;
+        Invoke("EndDash", dashTime);
+    }
+
+    void EndDash()
+    {
+        isDashing = false;
+        rigid.velocity = Vector2.zero;
+        rigid.gravityScale = defaultGravity;
+    }
+    /* 추가된 변수들 */
 }
