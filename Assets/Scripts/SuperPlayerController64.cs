924e7f498c6543b24db14193ee04166b9ab6e5a5
JWK258 <rlawodnjs200@hs.ac.kr>
2025-11-27 22:16:09 +0900

    Add files via upload
    점프 및 공격 동작 관련 미세 수정

--- Assets/Scripts/SuperPlayerController64.cs
+++ Assets/Scripts/SuperPlayerController64.cs
@@ -28,8 +28,8 @@ public class SuperPlayerController64 : MonoBehaviour
         Vector2 vec;
             float i = h; // 전 프레임 입력값 가져와서 회전 자연스럽게 하기
             if (Input.GetButtonUp("Horizontal") || Input.GetButton("Horizontal")) // 스프라이트 뒤집기
-                sprend.flipX = i > 0;
-            h = Input.GetAxisRaw("Horizontal");
+                sprend.flipX = i < 0;
+            h = Input.GetAxisRaw("Horizontal"); // 순서상 갱신 되기 전에 위의 i에서 값을 가져옴 -> 1프레임 짤짤이는 방향을 꼬아버릴 가능성. 다른 방식으로 플립하기 고려
             vec = new Vector2(h*3, rigid.velocity.y);
         // rigid.AddForce(vec, ForceMode2D.Impulse); // 이동
         rigid.velocity = vec;
@@ -39,14 +39,14 @@ public class SuperPlayerController64 : MonoBehaviour
             Vector2 vec2 = new Vector2(rigid.velocity.x, 10);
             rigid.velocity = vec2;
         }
-        if (rigid.velocity.normalized.y == 0)
+        if (rigid.velocity.normalized.y <= 0.01f && rigid.velocity.normalized.y >= -0.01f) // 약간의 y값 이동오차를 무시, 나중에 점프 판정은 조금 수정 필요함
         {
             anim.SetBool("Jump", false);
         }
         else
             anim.SetBool("Jump", true);
 
-        if (rigid.velocity.normalized.x == 0) // 속도 단위벡터 값이 0 이면 정지
+        if (rigid.velocity.normalized.x == 0) // 속도 단위벡터 값이 0 이면 정지로 취급
         {
             anim.SetBool("Walking", false);
         }
@@ -64,8 +64,8 @@ public class SuperPlayerController64 : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if (rigid.velocity.x > maxSpeed) // 오른쪽이동
+        if (rigid.velocity.x > maxSpeed && anim.GetBool("Attack")==false) // 오른쪽이동
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y); 
-        else if (rigid.velocity.x < -1 * maxSpeed) //왼쪽이동
+        else if (rigid.velocity.x < -1 * maxSpeed && anim.GetBool("Attack") == false) //왼쪽이동
             rigid.velocity = new Vector2(-1 * maxSpeed, rigid.velocity.y); }
 }
